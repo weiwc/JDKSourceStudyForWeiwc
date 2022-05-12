1ec8b1abbe4d7e712010881b2321cabc1ecf1ef0
威宸 <weiwc816@gmail.com>
2022-05-12 22:48:11 +0800

    init jdk 1.8u

---
+++ src/MainTest.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+/**
+ * @author weichen
+ * @description: TODO
+ * @create 2022-05-12 21:48
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("k1", "data");
+    }
+}
